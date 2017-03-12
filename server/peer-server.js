a05aa1cb6e42e3f50b9f43fcc2bd71dbb4aa6255
Bogdan Pshonyak <bpshonyak@live.com>
2017-03-12 03:51:12 -0700

    added temperary storage for user id's on the server

--- server/peer-server.js
+++ server/peer-server.js
@@ -1,2 +1,23 @@
 var PeerServer = require('peer').PeerServer;
 var server = PeerServer({port: 9000, path: '/peerjs'});
+
+var activeUsers = [];
+
+function addUser(id) {
+    activeUsers.push(id);
+}
+
+function removeUser(id) {
+    var userIdIndex = activeUsers.indexOf(id);
+    activeUsers.splice(userIdIndex, 1);
+}
+
+server.on('connection', function(id) { 
+    console.log('Joining: ', id);
+    addUser(id);
+});
+
+server.on('disconnect', function(id) {
+    console.log('Leaving: ', id);
+    removeUser(id);
+ });
